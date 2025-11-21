24d633a4a069c6d59c8d56f5036226a71f71034c
Juliano Penna <julianopenna@gmail.com>
2025-11-21 14:34:20 -0300

    Test script with values

--- examples/simple.js
+++ examples/simple.js
@@ -5,9 +5,9 @@ var winston = require('winston'),
 // when you don't provide a name the default one
 // is CloudWatch
 winston.add(new WinstonCloudWatch({
-  logGroupName: 'testing',
-  logStreamName: 'first',
-  awsRegion: 'us-east-1'
+  logGroupName: '/test/jp-logging',
+  logStreamName: 'logging-test-1',
+  awsRegion: 'us-west-2'
 }));
 
 winston.error('1');
